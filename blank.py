382b3fcda3443f77bb22b3b3804d9297eb3dbbc8
rejoysb <rejoysuresh@gmail.com>
2023-04-06 17:37:09 -0500

    first chapter

---
+++ blank.py
@@ -0,0 +1,12 @@
+print("my first python program")
+
+print("\n enter your name")
+
+name = input()
+
+print("my name is " + str(name))
+
+print("length of your name "+ str(len(str(name))))
+
+#so this is how you write commands in python
+
