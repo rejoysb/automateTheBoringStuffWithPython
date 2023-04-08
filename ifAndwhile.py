4eafaa349348aa477560b7affbe0538a3147159b
rejoysb <rejoysuresh@gmail.com>
2023-04-08 07:40:21 -0500

    practiced if, while and for loope in range and increments

---
+++ ifAndwhile.py
@@ -0,0 +1,22 @@
+print("enter your name")
+name =input()
+if name=="Rejoy":
+    print("hello Word")
+
+elif name=="Reethu":
+    print ("Hello USA")
+
+scopeVal =0
+
+while scopeVal ==5:
+    print("while loop execution")
+    scopeVal += 1
+
+for num in range (10):
+    print("testing for loop in range")
+
+for num in range(11,25):
+    print("testing for loop with start and stop")
+
+for num in range (0, 10, 3):
+    print("this counts from 0 to 10 in intervals of 3 " + str(num))
